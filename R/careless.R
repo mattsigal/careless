b0549a88bde64c35ab7cb86e25e3ddf7ec7e722d
Matthew Sigal <matthewsigal@gmail.com>
2015-04-22 14:54:19 -0400

    First commit.

---
+++ R/careless.R
@@ -0,0 +1,10 @@
+#' careless: A package for identifying careless responding.
+#'
+#' The careless package provides a variety of metrics for identifying careless responding.
+#' Inititally, these are drawn from the statistics used by Craig and Meade (2012).
+#'
+#' @section careless functions: The careless functions ...
+#'
+#' @docType package
+#' @name careless
+NULL
