e3e94286d0197f5be238f1f6223ac68d3e034ea8
mattsigal <matthewsigal@gmail.com>
2015-04-29 11:37:08 -0400

    Updated documentation.

--- R/careless.R
+++ R/careless.R
@@ -3,7 +3,9 @@
 #' The careless package provides a variety of metrics for identifying careless responding.
 #' This these are drawn from the statistics used by Craig and Meade (2012).
 #'
-#' @section careless functions: The careless functions ...
+#' @section careless functions: So far, this package implements the following functions:
+#' `longString()`, which returns either the count or the value of the longest set of 
+#' repititions in a dataframe.
 #'
 #' @docType package
 #' @name careless
