11d5b9a2acfde6e6a309520ad00ff29789865e57
mattsigal <matthewsigal@gmail.com>
2015-04-29 15:38:07 -0400

    Add careless() convenience function.

--- R/careless.R
+++ R/careless.R
@@ -1,12 +1,42 @@
-#' careless: Metrics for identifying careless responding.
+#' Careless Respondants
 #'
-#' The careless package provides a variety of metrics for identifying careless responding.
-#' This these are drawn from the statistics used by Craig and Meade (2012).
+#' \code{careless} is a summary function that calls various functions within the `careless` package.
 #'
-#' @section careless functions: So far, this package implements the following functions:
-#' `longString()`, which returns either the count or the value of the longest set of 
-#' repititions in a dataframe.
+#' This function is designed to work with a dataframe or matrix object.
 #'
-#' @docType package
-#' @name careless
-NULL
+#' @param x An `R` dataframe or matrix object.
+#' @param append A boolean scalar. If `append = TRUE`, the original dataframe is returned with the
+#' careless responding metrics bound to it. If `append = FALSE`, only the careless responding
+#' metrics are returned.
+#' @param na.rm A logical scalar. This is passed to \code{longString()}.
+#' 
+#' @return A dataframe object.
+#' 
+#' @export
+#' 
+#' @examples
+#' \dontrun{
+#' set.seed(77)
+#' dat <- matrix(sample(1:3, 200, replace = TRUE),20,10)
+#' stats <- careless(dat, append = FALSE)
+#' dat <- careless(dat, append = TRUE)
+#' }
+#' 
+#' @seealso \code{\link{longString}},\code{\link{malDist}}
+
+careless <- function(x, append = TRUE, na.rm = FALSE){
+  if (is.matrix(x)){
+    x <- as.data.frame(x)
+  }
+
+  if (!is.data.frame(x))
+    stop("input must be a data.frame or matrix object")
+  
+  longString <- longString(x, na.rm, return.value = FALSE)
+  longStringValue <- longString(x, na.rm, return.value = TRUE)
+  MahalanobisD <- malDist(x)
+  
+  out <- data.frame(longString, longStringValue, MahalanobisD)
+  
+  if (append) return(data.frame(x, out)) else return(out)
+}
--- R/malDist.R
+++ R/malDist.R
@@ -3,14 +3,18 @@
 #' \code{malDist} returns a vector indicating the Mahalanobis Distance for each participant in a
 #' dataframe.
 #'
-#' This function is designed for univariate tests, and assumes that all variables passed belong
-#' to the same factor. Also, if data are passed as factors, it is converted to integer before
+#' By default, this function is designed for univariate tests, and assumes that all variables 
+#' passed belong to the same factor. However, see nitems if there is a multidimensional
+#' structure. Note: if the data are passed as factors, it is converted to integer before
 #' calculating the Mahalanobis Distance. If missing data is present, the function will return
 #' NA for that observation.
 #'
 #' @param x An `R` dataframe or matrix object.
+#' @param nitems An integer. If dataset is multivariate, indicate the number of items per scale.
+#' At present, this is only defined for factors assessed with the same number of indicators.
 #' 
-#' @return A vector of type numeric.
+#' @return A vector of type numeric. If nitems is given, the return is of type matrix and contains
+#' one vector per factor (D1...Dn), as well as their average (AverageD).
 #' 
 #' @export
 #' 
@@ -19,11 +23,12 @@
 #' set.seed(77)
 #' dat <- matrix(sample(1:3, 200, replace = TRUE),20,10)
 #' malDist(dat)
+#' malDist(dat, nitems = 5)
 #' }
 #' 
 #' @seealso \code{\link{longString}}
 #' 
-malDist <- function(x){
+malDist <- function(x, nitems = NA){
   if (is.matrix(x)){
     x <- as.data.frame(x)
   }
@@ -37,8 +42,24 @@ malDist <- function(x){
     }
   }
 
-  meanVec <- colMeans(x, na.rm = TRUE)
-  varMat <- var(x, na.rm = TRUE)
-  out <- unname(mahalanobis(x, meanVec, varMat))
-  return(out)
+  if (is.na(nitems)){
+    # Default, nitems is NA:
+    meanVec <- colMeans(x, na.rm = TRUE)
+    varMat <- var(x, na.rm = TRUE)
+    out <- unname(mahalanobis(x, meanVec, varMat))
+    return(out)
+  } else {
+    # If nitems is given:
+    out <- matrix(NA, nrow = nrow(x), ncol = (ncol(x)/nitems))
+    itemGroups <- matrix(data = 1:length(x), nrow = nitems)
+    for (i in 1:ncol(itemGroups)){
+      subsetDat <- x[,itemGroups[,i]]
+      meanVec <- colMeans(subsetDat, na.rm = TRUE)
+      varMat <- var(subsetDat, na.rm = TRUE)
+      out[,i] <- unname(mahalanobis(subsetDat, meanVec, varMat))
+    }
+    out <- cbind(out, rowMeans(out))
+    colnames(out) <- c(paste0("D", 1:(ncol(out)-1)), "AverageD")
+    return(out)
+  }
 }
